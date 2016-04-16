2cbad525a135c5c4493600bc860ae55046330c6e
Magnus Bellstrand <bellstrand@gmail.com>
2016-04-16 17:46:18 +0200

    seed hashed password

--- seed/data/users.js
+++ seed/data/users.js
@@ -2,7 +2,7 @@ module.exports = {
 	model: 'Users',
 	data: [{
 		username: 'admin',
-		password: 'password',
+		password: '$2a$10$i8l2X50/pPtJRAWXW.E.a.rMynSV8hBDnOTPD6P/XopMYE8MHQ/66', // password
 		name: 'Admin',
 		email: '',
 		phone: '',
